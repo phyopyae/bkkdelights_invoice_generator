e7aafebf1f65aa60cb2c879daf71614aaa6572aa
phyopyae.dev.mm <phyopyae2@gmail.com>
2025-01-28 02:06:03 +0700

    Added findByInvoiceNumber and deleteByInvoiceNumber for delete function

--- src/main/java/com/psychojunior/invoice/template/repository/InvoiceItemRepository.java
+++ src/main/java/com/psychojunior/invoice/template/repository/InvoiceItemRepository.java
@@ -1,9 +1,16 @@
 package com.psychojunior.invoice.template.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.psychojunior.invoice.template.entity.InvoiceItem;
 
 public interface InvoiceItemRepository  extends JpaRepository<InvoiceItem, Long>  {
 
+	List<InvoiceItem> findByInvoiceNumber(String invoiceNumber);
+	
+	@Transactional
+	void deleteByInvoiceNumber(String invoiceNumber);
 }
--- src/main/java/com/psychojunior/invoice/template/repository/InvoiceRepository.java
+++ src/main/java/com/psychojunior/invoice/template/repository/InvoiceRepository.java
@@ -1,11 +1,15 @@
 package com.psychojunior.invoice.template.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.psychojunior.invoice.template.entity.Invoice;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long>  {
 
+	@SuppressWarnings("unchecked")
 	Invoice save(Invoice invoice);
-
+	
+	Optional<Invoice> findByInvoiceNumber(String invoiceNumber);
 }
