46eb2e7088fa2133aa4a60252cd321e8104e8e37
phyopyae.dev.mm <phyopyae2@gmail.com>
2026-06-14 00:21:49 +0630

    Removed ROLE from default role name

--- src/main/java/com/psychojunior/invoice/template/constant/InvoiceConstant.java
+++ src/main/java/com/psychojunior/invoice/template/constant/InvoiceConstant.java
@@ -2,6 +2,6 @@ package com.psychojunior.invoice.template.constant;
 
 public class InvoiceConstant {
 
-	public static final String ROLE_USER = "ROLE_USER";
-	public static final String ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String ROLE_USER = "USER";
+	public static final String ROLE_ADMIN = "ADMIN";
 }
